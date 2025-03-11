a4b2abc1a4f4b41f87c2e634dc1fec90d2ba7667
F1Soda <golik.tima@gmail.com>
2025-03-11 16:43:44 +0500

    full five and sex

--- index.js
+++ index.js
@@ -69,12 +69,12 @@ function getTransform(translation, ratio) {
 function animaster() {
     return {
         fadeIn(element, duration) {
-            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transitionDuration = `${duration}ms`;
             element.classList.remove('hide');
             element.classList.add('show');
         },
         fadeOut(element, duration) {
-            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transitionDuration = `${duration}ms`;
             element.classList.remove('show');
             element.classList.add('hide');
         },
@@ -83,40 +83,40 @@ function animaster() {
             element.style.transform = getTransform(translation, null);
         },
         scale(element, duration, ratio) {
-            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
         },
-        moveAndHide(element, duration)
-        {
+        moveAndHide(element, duration) {
             element.style.transitionDuration = `${duration * 2.0 / 5}ms`;
             element.style.transform = getTransform({x: 100, y: 20}, null);
             setTimeout(animaster().fadeOut, duration * 2.0 / 5, element, duration * 3 / 5);
         },
         heartBeating(element) {
-        let scaleUp = true;
+            let scaleUp = true;
 
-        let a = 1;
-        function animateScale() {
-            if (scaleUp) {
-                animaster().scale(element, 500, a);
-            } else {
-                animaster().scale(element, 500, 1);
-            }
-            scaleUp = !scaleUp;
-            a += 0.05;
-        }
+            let a = 1;
 
-        const intervalId = setInterval(animateScale, 500);
-        return {
-            stop: () => {
-                clearInterval(intervalId);
-                animaster().scale(element, 500, 1);
+            function animateScale() {
+                if (scaleUp) {
+                    animaster().scale(element, 500, a);
+                } else {
+                    animaster().scale(element, 500, 1);
+                }
+                scaleUp = !scaleUp;
+                a += 0.05;
             }
-        };
-    },
+
+            const intervalId = setInterval(animateScale, 500);
+            return {
+                stop: () => {
+                    clearInterval(intervalId);
+                    animaster().scale(element, 500, 1);
+                }
+            };
+        },
         showAndHide(element, duration) {
-            animaster().fadeIn(element, duration * 1/3);
-            setInterval(animaster().fadeOut, duration * 1/3, element, duration * 1 / 3);
+            animaster().fadeIn(element, duration * 1 / 3);
+            setInterval(animaster().fadeOut, duration * 1 / 3, element, duration * 1 / 3);
         }
     }
 }
